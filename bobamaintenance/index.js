d02937e426b5505a5d19841cd5cfa98bddb90ae9
Jeff B <105983864+Jeff-Shango@users.noreply.github.com>
2023-03-13 22:25:02 -0400

    update

--- bobamaintenance/index.js
+++ bobamaintenance/index.js
@@ -1,4 +1,14 @@
-const app= require('express')();
+import express from "express";
+import mysql from 'mysql2';
+
+const app= express();
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "baltimore",
+    database: "Client_Server"
+})
 
 const PORT = 8000;
 
@@ -8,9 +18,18 @@ app.listen(
 );
 
 app.get('/getRequest', (req, res) => {
-    res.send("successful get request")
+    const q = "SELECT * FROM Client_Server.clientInformation"
+    // res.send("successful get request")
+    db.query(q,(err, data) => {
+        if(err) return res.json(err)
+        return res.json(data)
+    })
 });
 
 app.post('/postRequest', (req, res) => {
     res.send("Successful post request.. it worked")
+})
+
+app.put('/putRequest', (req, res) => {
+    res.send("INSERT INTO clientInformation VALUES")
 })
